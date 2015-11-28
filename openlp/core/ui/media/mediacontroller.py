712bb6c0750ee9ef75b905e2684cbdf2afd9b4c4
Tim Bentley <tim.bentley@gmail.com>
2015-11-28 15:28:43 +0000

    Fix up missing players for Presentations and Media.

--- openlp/core/ui/media/mediacontroller.py
+++ openlp/core/ui/media/mediacontroller.py
@@ -514,10 +514,15 @@ class MediaController(RegistryMixin, OpenLPMixin, RegistryProperties):
         :param display: Which display to use
         :param service_item: The ServiceItem containing the details to be played.
         """
-        used_players = get_media_players()[0]
+        used_players = get_media_players()
+        default_player = used_players[0]
         if service_item.processor and service_item.processor != UiStrings().Automatic:
-            used_players = [service_item.processor.lower()]
-        # If no player, we can't play
+            # check to see if the player is usable else use the default one.
+            if not service_item.processor.lower() in used_players:
+                used_players = default_player
+            else:
+                used_players = [service_item.processor.lower()]
+
         if not used_players:
             return False
         if controller.media_info.file_info.isFile():
